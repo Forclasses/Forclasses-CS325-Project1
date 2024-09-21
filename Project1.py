8b6915abcb810c83c23b3bd9adc6d305515c1ab4
Forclasses <brucec22180@gmail.com>
2024-09-21 13:39:25 -0500

    Added start of input code and have a test for it

--- Project1.py
+++ Project1.py
@@ -2,3 +2,13 @@
 # Need, to export strings from another file and put them into
 #a slm then  take the output from that ansd put it into another
 #file
+
+
+#start with getting the text from the first file
+
+file_path = "C:\CS325-Project1\input.txt"
+
+with open(file_path, 'r') as file:
+    file_content = file.read()
+
+print(file_content) #making sure this functions
